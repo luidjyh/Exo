4f79a16e572a5318e1cdd6eb84bbbdf6297c602a
luidjyh <38428237+luidjyh@users.noreply.github.com>
2018-04-19 13:37:34 +0200

    Add files via upload

---
+++ Exo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#exo n°1
+print('Je dois faire des sauvegardes régulières de mes fichiers.' * 500)
+
+#Exo2
+print(*range(0,1000,3))
+
+#Exo3
+import pprint
+n = 13
+m = list(list(range(1*i,(n+1)*i, i)) for i in range(1,n+1))
+pprint.pprint(m)
+
+#Exo4
+input("Enter your name: ")
+len(input)
+
+#Exo5
+
+#Exo6
+
+#Exo7
+
+#Exo8
+
+#Exo10
+
+#Exo11
+
+#Exo12
+
+#Exo13 
